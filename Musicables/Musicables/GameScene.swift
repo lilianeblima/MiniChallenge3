71ee20cf0f8b10520e76dd497cec71f7583c8e51
Eduardo Quadros <quadros.ea@gmail.com>
2015-05-25 09:12:55 -0300

    Draw the stave and add the menu intro.

--- Musicables/Musicables/GameScene.swift
+++ Musicables/Musicables/GameScene.swift
@@ -13,9 +13,71 @@ var refer2: SKSpriteNode!
 var note: SKSpriteNode!
 var cleanButton: SKSpriteNode!
 
+enum StaveElements {
+    case Line
+    case Space
+}
+
 class GameScene: SKScene {
+
+    // MARK: - Constants
+
+    // Declare stave constants.
+    private let partialnumberOfLines = 5
+    private let totalNumberOfLines = 7
+    private let partialNumberOfSpaces = 4
+    private let totalNumberOfSpaces = 6
+
+    // The stave takes about a third of the screen's height,
+    // so both top and bottom margins measures 256 points.
+    private let bottomMargin: CGFloat = 256.0
+    private let topMargin: CGFloat = 256.0
+
+    // Arbitrary X-axis offset distance and elements length.
+    private let leftMargin: CGFloat = 100.0
+    private let lineLength: CGFloat = 10000.0
+
+    // Declare stave dimension.
+    private let lineWidth: CGFloat = 16.0
+    private let spaceWidth: CGFloat = 48.0
+
+    // Y-Axis note point coordinates equally distributed.
+    private let YCoordinates = [
+        192.0,
+        224.0,
+        256.0,
+        288.0,
+        320.0,
+        352.0,
+        384.0,
+        416.0,
+        448.0,
+        480.0,
+        512.0,
+        544.0,
+        576.0
+    ]
+
+    // Color schemes.
+    private let staveColors = [
+        UIColor(red: 0.694, green: 0.0, blue: 1.0, alpha: 1.0), // line
+        UIColor(red: 0.435, green: 1.0, blue: 0.651, alpha: 1.0), // space
+        UIColor(red: 0.584, green: 0.0, blue: 1.0, alpha: 1.0), // line
+        UIColor(red: 0.033, green: 1.0, blue: 0.671, alpha: 1.0), // space
+        UIColor(red: 0.047, green: 0.706, blue: 1.0, alpha: 1.0), // line
+        UIColor(red: 1.0, green: 0.988, blue: 0.333, alpha: 1.0), // space
+        UIColor(red: 0.027, green: 1.0, blue: 0.0, alpha: 1.0), // line
+        UIColor(red: 1.0, green: 0.659, blue: 0.376, alpha: 1.0), // space
+        UIColor(red: 1.0, green: 0.808, blue: 0.047, alpha: 1.0), // line
+        UIColor(red: 0.333, green: 0.455, blue: 1.0, alpha: 1.0), // space
+        UIColor(red: 1.0, green: 0.2, blue: 0.0, alpha: 1.0), // line
+        UIColor(red: 1.0, green: 0.475, blue: 0.832, alpha: 1.0), // space
+        UIColor(red: 1.0, green: 0.0, blue: 0.424, alpha: 1.0), // line
+    ]
+
+    // MARK: - View Creation
+
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
 
         self.backgroundColor = SKColor.whiteColor()
         
@@ -34,6 +96,45 @@ class GameScene: SKScene {
         addChild(refer2)
         
         addCleanButton()
+
+        drawStave()
+    }
+
+    private func drawContinuousLine(#YCoordinate: CGFloat, shape: StaveElements, color: UIColor) -> SKShapeNode {
+        var startPoint = CGPointMake(leftMargin, YCoordinate)
+        var endPoint = CGPointMake(lineLength, YCoordinate)
+        var points = [startPoint, endPoint]
+        var line = SKShapeNode(points: &points, count: points.count)
+
+        if shape == StaveElements.Line {
+            line.lineWidth = lineWidth
+            line.lineCap = kCGLineCapRound
+            line.glowWidth = 1.0
+            line.strokeColor = color
+        } else {
+            line.lineWidth = spaceWidth
+            line.lineCap = kCGLineCapButt
+            line.glowWidth = 0
+            line.strokeColor = color
+        }
+
+        return line
+    }
+
+    private func drawStave() {
+        for index in 2...10 {
+            var shape: SKShapeNode
+            var YCoordinate = CGFloat(YCoordinates[index])
+            var color = staveColors[index]
+
+            if (index % 2) == 0 {
+                shape = drawContinuousLine(YCoordinate: YCoordinate, shape: StaveElements.Line, color: color)
+            } else {
+                shape = drawContinuousLine(YCoordinate: YCoordinate, shape: StaveElements.Space, color: color)
+            }
+
+            addChild(shape)
+        }
     }
     
     func addCleanButton(){
